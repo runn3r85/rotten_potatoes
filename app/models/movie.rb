c817c25b3091f9d059d56fd8461a978541358a16
Brandon Barrette <brandon.barrette@gmail.com>
2015-05-04 15:19:51 -0700

    Day 5 add font-awesome and restyle login page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
   has_many :reviews
 
-  has_attached_file :movie_cover, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :movie_cover, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "missing_movie.png"
   validates_attachment_content_type :movie_cover, :content_type => /\Aimage\/.*\Z/
 
   validates :title, :body, :year, presence: true
